c22b194aac4d76f1f298d382a1b02aedf28df889
Alexandra Mompremier <amompremier@gmail.com>
2024-07-24 13:13:28 -0500

    refactored comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
-  before_action :is_an_authorized_user, only: [:destroy, :create]
+  before_action -> { authorize @comment || Comment }, except: [:new, :create]
 
   # GET /comments or /comments.json
   def index
@@ -13,6 +13,8 @@ class CommentsController < ApplicationController
 
   # GET /comments/new
   def new
+    @photo = Photo.find(params[:comment][:photo_id])
+    authorize @photo, :show?
     @comment = Comment.new
   end
 
@@ -22,38 +24,32 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
+    @photo = Photo.find(params[:comment][:photo_id])
+    authorize @photo, :show?
     @comment = Comment.new(comment_params)
     @comment.author = current_user
 
-    if @comment.author = current_user
-      respond_to do |format|
-        if @comment.save
-          format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
-          format.json { render :show, status: :created, location: @comment }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @comment.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    else
-      redirect_back(fallback_location: root_url, alert: "You cannot edit comments from others!")
     end
   end
 
   # PATCH/PUT /comments/1 or /comments/1.json
   def update
-    if @comment.author == current_user
-      respond_to do |format|
-        if @comment.update(comment_params)
-          format.html { redirect_to root_url, notice: "Comment was successfully updated." }
-          format.json { render :show, status: :ok, location: @comment }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @comment.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to root_url, notice: "Comment was successfully updated." }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    else
-      redirect_back(fallback_location: root_url, alert: "Tsk, tsk! No editing other's comments!!")
     end
   end
 
@@ -67,21 +63,13 @@ class CommentsController < ApplicationController
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def comment_params
-    params.require(:comment).permit(:author_id, :photo_id, :body)
-  end
-
-  def is_an_authorized_user
-    @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
-    if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
-      redirect_back fallback_location: root_url, alert: "Not authorized!"
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
-  end
+
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:author_id, :photo_id, :body)
+    end
 end
